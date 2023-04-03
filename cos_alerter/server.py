1e1027ac8654b58b985b63de8a2f88b7a284b1f0
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2023-04-03 17:25:52 +0300

    use monotonic clock, use UTC, and move some logic in to the AlerterState class (#28)
    * use monotonic clock, use UTC, and move some logic in to the AlerterState class
    
    * Simplify code based on suggestions.
    
    Co-authored-by: Leon <82407168+sed-i@users.noreply.github.com>
    
    * python 3.11 is no longer required
    
    * break out some new methods
    
    * forgot to remove a line
    
    * Instantiate AlerterState once
    
    * Fix typo
    
    Co-authored-by: PietroPasotti <pietro.pasotti@canonical.com>
    
    * better method name
    
    ---------
    
    Co-authored-by: Leon <82407168+sed-i@users.noreply.github.com>
    Co-authored-by: PietroPasotti <pietro.pasotti@canonical.com>

--- cos_alerter/server.py
+++ cos_alerter/server.py
@@ -1,18 +1,13 @@
-import time
-import yaml
-
 from flask import Flask
 
 from .alerter import AlerterState
 
 app = Flask(__name__)
-with open('/etc/cos-alerter.yaml', 'rb') as f:
-    app.config.update(yaml.safe_load(f))
 
 
 @app.route('/alive', methods=['POST'])
 def alive():
     # TODO Decide if we should validate the request.
-    with AlerterState() as data:
-        data.alert_time = time.time()
+    with AlerterState() as state:
+        state.reset_alert_timeout()
     return 'Success!'
